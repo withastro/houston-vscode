9aae07837106b4442d799742b37fdbc7a72da708
Nate Moore <nate@astro.build>
2022-12-12 19:35:21 -0600

    v0.0.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,9 @@
 # Change Log
 
-All notable changes to the "houston" extension will be documented in this file.
+## v0.0.3
 
-Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
+- Tweaks to ANSI colors, sidebar colors
 
-## [Unreleased]
+## v0.0.1
 
 - Initial release
--- src/extension.ts
+++ src/extension.ts
@@ -1,114 +1,114 @@
 import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
-  const provider = new HoustonViewProvider(context.extensionUri);
+	const provider = new HoustonViewProvider(context.extensionUri);
 
-  context.subscriptions.push(
-    vscode.window.registerWebviewViewProvider(
-      HoustonViewProvider.viewType,
-      provider
-    )
-  );
+	context.subscriptions.push(
+		vscode.window.registerWebviewViewProvider(
+			HoustonViewProvider.viewType,
+			provider
+		)
+	);
 
-  if (vscode.window.activeTextEditor) {
-    updateDiagnostics(provider);
-  }
+	if (vscode.window.activeTextEditor) {
+		updateDiagnostics(provider);
+	}
 
-  context.subscriptions.push(
+	context.subscriptions.push(
 		vscode.languages.onDidChangeDiagnostics(() => updateDiagnostics(provider))
 	);
 }
 
 function updateDiagnostics(provider: HoustonViewProvider): void {
-  const diagnostics = vscode.languages.getDiagnostics();
-  let problems = 0;
-  for (const [doc, collection] of diagnostics) {
-    problems += collection.length;
-  }
+	const diagnostics = vscode.languages.getDiagnostics();
+	let problems = 0;
+	for (const [doc, collection] of diagnostics) {
+		problems += collection.length;
+	}
 
-  if (problems <= 0) {
-    if (!['happy', 'default'].includes(provider.pose)) {
-      provider.setPose("happy");
-      setTimeout(() => {
-        provider.setPose("default");
-      }, 600);
-    } else {
-      provider.setPose("default");
-    }
-  } else if (problems === 1) {
-    provider.setPose("sad");
-  } else if (problems <= 3) {
-    provider.setPose("disappointed");
-  } else if (problems <= 6) {
-    provider.setPose("shocked");
-  } else {
-    provider.setPose("cry");
-  }
+	if (problems <= 0) {
+		if (!["happy", "default"].includes(provider.pose)) {
+			provider.setPose("happy");
+			setTimeout(() => {
+				provider.setPose("default");
+			}, 600);
+		} else {
+			provider.setPose("default");
+		}
+	} else if (problems === 1) {
+		provider.setPose("sad");
+	} else if (problems <= 3) {
+		provider.setPose("disappointed");
+	} else if (problems <= 6) {
+		provider.setPose("shocked");
+	} else {
+		provider.setPose("cry");
+	}
 }
 
 class HoustonViewProvider implements vscode.WebviewViewProvider {
-  public static readonly viewType = "houston.hello";
+	public static readonly viewType = "houston.hello";
 
-  public pose: string = 'default';
-  private _view?: vscode.WebviewView;
+	public pose: string = "default";
+	private _view?: vscode.WebviewView;
 
-  constructor(private readonly _extensionUri: vscode.Uri) {}
+	constructor(private readonly _extensionUri: vscode.Uri) {}
 
-  public resolveWebviewView(
-    webviewView: vscode.WebviewView,
-    context: vscode.WebviewViewResolveContext,
-    _token: vscode.CancellationToken
-  ) {
-    this._view = webviewView;
+	public resolveWebviewView(
+		webviewView: vscode.WebviewView,
+		context: vscode.WebviewViewResolveContext,
+		_token: vscode.CancellationToken
+	) {
+		this._view = webviewView;
 
-    webviewView.webview.options = {
-      // Allow scripts in the webview
-      enableScripts: true,
+		webviewView.webview.options = {
+			// Allow scripts in the webview
+			enableScripts: true,
 
-      localResourceRoots: [this._extensionUri],
-    };
+			localResourceRoots: [this._extensionUri],
+		};
 
-    webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
+		webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
 
-    // webviewView.webview.onDidReceiveMessage(data => {
-    // 	switch (data.type) {
-    // 		case 'colorSelected':
-    // 			{
-    // 				vscode.window.activeTextEditor?.insertSnippet(new vscode.SnippetString(`#${data.value}`));
-    // 				break;
-    // 			}
-    // 	}
-    // });
-  }
+		// webviewView.webview.onDidReceiveMessage(data => {
+		// 	switch (data.type) {
+		// 		case 'colorSelected':
+		// 			{
+		// 				vscode.window.activeTextEditor?.insertSnippet(new vscode.SnippetString(`#${data.value}`));
+		// 				break;
+		// 			}
+		// 	}
+		// });
+	}
 
-  public setPose(pose: string) {
-    if (this._view) {
-      this._view.webview.postMessage({ type: "pose", pose });
-      this.pose = pose;
-    }
-  }
+	public setPose(pose: string) {
+		if (this._view) {
+			this._view.webview.postMessage({ type: "pose", pose });
+			this.pose = pose;
+		}
+	}
 
-  private _getHtmlForWebview(webview: vscode.Webview) {
-    // Get the local path to main script run in the webview, then convert it to a uri we can use in the webview.
-    const scriptUri = webview.asWebviewUri(
-      vscode.Uri.joinPath(this._extensionUri, "media", "main.js")
-    );
+	private _getHtmlForWebview(webview: vscode.Webview) {
+		// Get the local path to main script run in the webview, then convert it to a uri we can use in the webview.
+		const scriptUri = webview.asWebviewUri(
+			vscode.Uri.joinPath(this._extensionUri, "media", "main.js")
+		);
 
-    // Do the same for the stylesheet.
-    const styleResetUri = webview.asWebviewUri(
-      vscode.Uri.joinPath(this._extensionUri, "media", "reset.css")
-    );
-    const styleVSCodeUri = webview.asWebviewUri(
-      vscode.Uri.joinPath(this._extensionUri, "media", "vscode.css")
-    );
-    const styleMainUri = webview.asWebviewUri(
-      vscode.Uri.joinPath(this._extensionUri, "media", "main.css")
-    );
+		// Do the same for the stylesheet.
+		const styleResetUri = webview.asWebviewUri(
+			vscode.Uri.joinPath(this._extensionUri, "media", "reset.css")
+		);
+		const styleVSCodeUri = webview.asWebviewUri(
+			vscode.Uri.joinPath(this._extensionUri, "media", "vscode.css")
+		);
+		const styleMainUri = webview.asWebviewUri(
+			vscode.Uri.joinPath(this._extensionUri, "media", "main.css")
+		);
 
-    // Use a nonce to only allow a specific script to be run.
-    const nonce = getNonce();
+		// Use a nonce to only allow a specific script to be run.
+		const nonce = getNonce();
 
-    return `<!DOCTYPE html>
+		return `<!DOCTYPE html>
 			<html lang="en">
 			<head>
 				<meta charset="UTF-8">
@@ -143,15 +143,15 @@ class HoustonViewProvider implements vscode.WebviewViewProvider {
 				<script type="module" nonce="${nonce}" src="${scriptUri}"></script>
 			</body>
 		</html>`;
-  }
+	}
 }
 
 function getNonce() {
-  let text = "";
-  const possible =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  for (let i = 0; i < 32; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return text;
+	let text = "";
+	const possible =
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	for (let i = 0; i < 32; i++) {
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+	}
+	return text;
 }
